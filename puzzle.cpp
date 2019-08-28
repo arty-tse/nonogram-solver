551b022293a2b577c733bf041c95d7f8b923236a
Arthur Tse <17014719+arty-tse@users.noreply.github.com>
2019-08-27 22:45:25 -0700

    Solve perfect clues

--- puzzle.cpp
+++ puzzle.cpp
@@ -1,7 +1,7 @@
 #include "puzzle.hpp"
 
 // Puzzle constructor
-Puzzle::Puzzle(int nCols, int nRows, std::vector<std::string> cols, std::vector<std::string> rows)
+Puzzle::Puzzle(int nCols, int nRows, std::vector<std::vector<int>> cols, std::vector<std::vector<int>> rows)
 {
     s_nCols = nCols;
     s_nRows = nRows;
@@ -12,15 +12,71 @@ Puzzle::Puzzle(int nCols, int nRows, std::vector<std::string> cols, std::vector<
 // solve a puzzle by drawing the solved puzzle
 std::vector<std::vector<char>> Puzzle::solve()
 {
+    // Initialize unsolved puzzle
     std::vector<std::vector<char>> solved;
     for (int i = 0; i < s_nRows; ++i)
     {
         std::vector<char> row;
         for (int j = 0; j < s_nCols; ++j)
         {
-            row.push_back('*');
+            row.push_back(' ');
         }
         solved.push_back(row);
+    }
+
+    // Solve what can be solved
+    for (int i = 0; i < s_nRows; ++i)
+    {
+        std::vector<int> clue = s_rows[i];
+        int sum = 0;
+        for (int j = 0; j < clue.size(); ++j)
+        {
+            sum += clue[j];
+            if (j > 0)
+            {
+                ++sum;
+            }
+        }
+        if (sum == s_nCols)
+        {
+            int j = 0;
+            for (int k = 0; k < clue.size(); ++k)
+            {
+                for (int l = 0; l < clue[k]; ++l)
+                {
+                    solved[i][j] = '*';
+                    ++j;
+                }
+                ++j;
+            }
+        }
+    }
+
+    for (int i = 0; i < s_nCols; ++i)
+    {
+        std::vector<int> clue = s_cols[i];
+        int sum = 0;
+        for (int j = 0; j < clue.size(); ++j)
+        {
+            sum += clue[j];
+            if (j > 0)
+            {
+                ++sum;
+            }
+        }
+        if (sum == s_nRows)
+        {
+            int j = 0;
+            for (int k = 0; k < clue.size(); ++k)
+            {
+                for (int l = 0; l < clue[k]; ++l)
+                {
+                    solved[j][i] = '*';
+                    ++j;
+                }
+                ++j;
+            }
+        }
     }
     return solved;
 }
--- puzzle.hpp
+++ puzzle.hpp
@@ -1,7 +1,6 @@
 #ifndef PUZZLE_H
 #define PUZZLE_H
 
-#include <string>
 #include <vector>
 
 class Puzzle
@@ -9,18 +8,18 @@ class Puzzle
 private:
   int s_nCols;
   int s_nRows;
-  std::vector<std::string> s_cols;
-  std::vector<std::string> s_rows;
+  std::vector<std::vector<int>> s_cols;
+  std::vector<std::vector<int>> s_rows;
 
 public:
-  Puzzle(int nCols, int nRows, std::vector<std::string> cols, std::vector<std::string> rows);
+  Puzzle(int nCols, int nRows, std::vector<std::vector<int>> cols, std::vector<std::vector<int>> rows);
 
   std::vector<std::vector<char>> solve();
 
   int getNumCols() { return s_nCols; }
   int getNumRows() { return s_nRows; }
-  std::vector<std::string> getCols() { return s_cols; }
-  std::vector<std::string> getRows() { return s_rows; }
+  std::vector<std::vector<int>> getCols() { return s_cols; }
+  std::vector<std::vector<int>> getRows() { return s_rows; }
 };
 
 #endif
